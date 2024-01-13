94db5e4b449792be9be81233d3a78eafa702ec25
Mir Okon <141348804+mir-okon@users.noreply.github.com>
2024-01-14 00:20:30 +0300

    Update db.sh

--- init/db.sh
+++ init/db.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 #OR !/bin/sh
 
+wget -O - https://raw.githubusercontent.com/mo31rus/script/alpine/install/base.sh | sh
 wget -O - https://raw.githubusercontent.com/mo31rus/script/alpine/install/hyper-v.sh | sh
