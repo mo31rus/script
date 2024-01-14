623ee1e1e63042d88341ab25f73a749cfa6915fe
Mir Okon <141348804+mir-okon@users.noreply.github.com>
2024-01-14 12:05:05 +0300

    Update db.sh

--- init/db.sh
+++ init/db.sh
@@ -3,3 +3,5 @@
 
 wget -O - https://raw.githubusercontent.com/mo31rus/script/alpine/install/base.sh | sh
 wget -O - https://raw.githubusercontent.com/mo31rus/script/alpine/install/hyper-v.sh | sh
+wget -O - https://raw.githubusercontent.com/mo31rus/script/alpine/install/raid.sh | sh
+wget -O - https://raw.githubusercontent.com/mo31rus/script/alpine/install/docker.sh | sh
